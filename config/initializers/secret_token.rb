bae628a17e96e8fae6051a76da4af321b04a33f5
Paul Liesse <pliesse@matcmadison.edu>
2012-03-04 20:37:40 -0600

    starting project 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Palatable::Application.config.secret_token = '08372b520c27fb77a23cb34ad87c054704d44bbdd3bc73248560ddf1968d3bb354946b94d1fbf49b6bd92dd3dd2fe0793f0586512fe1d99a4f7edda74d640b7c'
